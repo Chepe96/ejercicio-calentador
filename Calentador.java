be335fb6bab23618fe14302d16dd75df79f3ba33
José Luis Fernando Martínez Martínez <jlfmtz_96@alumnos.uaslp.edu.mx>
2017-08-23 18:57:26 -0500

    Se agregaron dos campo "min" y "max" para controlar la temperatura y los metodos de enfriar y calentar. Ademas de modificar el metodo de acceso en donde no returnaba la temperatura como estaba indicado.

--- Calentador.java
+++ Calentador.java
@@ -3,16 +3,20 @@ public class Calentador
     //variables de instancia
     private int temperatura;
     private int incremento;
+    private int max;
+    private int min;
     //Constructor
     public Calentador()
     {
         temperatura=15;
         incremento=3;
+        min=-6;
+        max=21;
     }
     //Metodos
     public void calentar()
     {
-        if(temperatura<=27)
+        if(temperatura<max)
         {
             temperatura=temperatura+incremento;
         }
@@ -20,14 +24,17 @@ public class Calentador
     
     public void enfriar()
     {
-        if(temperatura>=-7)
+        if(temperatura>min)
         {
             temperatura=temperatura-incremento;
         }
     }
     
-    public void consultaTemperatura()
-    {
-        System. out. println("\nLa temperatura ahora: "+temperatura+"°");
-    }
+   public String consultaTemp()
+   {
+       String cadResultado="";
+       cadResultado=cadResultado+temperatura;
+       cadResultado=cadResultado+"°";
+       return cadResultado;
+   }
 }
